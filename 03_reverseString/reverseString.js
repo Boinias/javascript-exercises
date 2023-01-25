8e3dd5f43e46d5f2dbe64f871e42c9c1663190aa
Jack Francis <jack.francis1100@gmail.com>
2023-01-25 12:03:57 +0000

    Completed it mate

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+/*
+Why on wearht does this commented out verwion not work???!!!
+const reverseString = function(word) {
+    return word
+    .split('')
+    .reverse()
+    .join('')
+};
 
+*/
+
+
+const reverseString = function(word) {
+    return word.split('').reverse().join('')
 };
 
 // Do not edit below this line
