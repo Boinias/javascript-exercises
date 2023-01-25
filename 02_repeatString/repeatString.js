ddaed581c62eb83b3108d816af025ca5443f08f4
Jack Francis <jack.francis1100@gmail.com>
2023-01-25 11:45:07 +0000

    idk

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+/*Why on earth does the first solution not pass? Idk */
 
-};
+/*const repeatString = function(word,times) {
+    let answer = ""
+    if (times < 0) {
+        answer = "ERROR"
+    } else {
+    let i = 0
+    while (i < times) {
+        answer += word
+    i++;
+    }
+    return answer
+}*/
+
+/*const repeatString = function(word,times) {
+    let answer = ""
+    if (times < 0) {
+        answer = "ERROR"
+    } else {
+    for (let i = 0; i < times; i++){
+        answer = answer + word
+    }}
+    return answer
+}*/
 
 // Do not edit below this line
 module.exports = repeatString;
