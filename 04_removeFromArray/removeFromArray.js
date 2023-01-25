a07ecbeae10a41e5faeaae7aee872f1732408e26
Jack Francis <jack.francis1100@gmail.com>
2023-01-25 17:27:09 +0000

    First test of removefromarray working

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const filtered = array.filter(forRemoval);
+    function forRemoval (value) {
+        return value != args;
+    }
+    return filtered
 };
 
 // Do not edit below this line
