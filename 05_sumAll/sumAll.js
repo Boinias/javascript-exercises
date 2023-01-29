21b01ddf83470c0a31084bb64891976ff3c9495f
Jack Francis <jack.francis1100@gmail.com>
2023-01-29 15:45:40 +0000

    fes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,13 +4,15 @@ const sumAll = function(a,b) {
     for (let i = a; a < b; i++) {
     answer += i
     }
-    return answer
     } else if (a > b) {
     for (let i = b; b < a;i++) {
         answer += i
     }
     }
+    return answer
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
