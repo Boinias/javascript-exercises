3b88ffbf1b6a01e9b6655a54649110635d6314c8
Jack Francis <jack.francis1100@gmail.com>
2023-01-29 15:37:33 +0000

    vds

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let answer;
+    if (a < b) {
+    for (let i = a; a < b; i++) {
+    answer += i
+    }
+    return answer
+    } else if (a > b) {
+    for (let i = b; b < a;i++) {
+        answer += i
+    }
+    }
 };
 
 // Do not edit below this line
