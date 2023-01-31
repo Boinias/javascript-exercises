b71526c793ace1a75fe7553984906160292b4aa5
Jack Francis <jack.francis1100@gmail.com>
2023-01-31 05:47:49 +0000

    idk

--- test2.js
+++ test2.js
@@ -1,15 +1,13 @@
 const sumAll = function(a,b) {
     let answer = 0
     if (a < b) {
-    for (let i = a; a < b; i++) {
+    for (let i = a; i <= b; i++) {
     answer += i
     }
     } else if (a > b) {
-    for (let i = b; b < a;i++) {
+    for (let i = b; i <= a;i++) {
         answer += i
     }
     }
     return answer
 };
-
-console.log(sumAll(1, 4));
