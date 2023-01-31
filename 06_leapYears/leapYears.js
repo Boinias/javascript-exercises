24587877a969dfd0376acb33d30799b40fd799af
Jack Francis <jack.francis1100@gmail.com>
2023-01-31 05:47:46 +0000

    idk

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%4==0) {
+    } else {
+        false
+    }
 };
 
+console.log (LEAPYEARS(1996))
+
 // Do not edit below this line
-module.exports = leapYears;
+// module.exports = leapYears;
+
+
+// if dividing by 4 is 0 then its a leap year
