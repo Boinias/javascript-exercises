3ad614d79750373710cdf978bbee0f0dd873711b
Jack Francis <jack.francis1100@gmail.com>
2023-03-02 11:34:51 +0000

    find oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
-};
+
+
+const findTheOldest = function() {
+let oldest;
+let oldestAge = 0
+
+for (i=0; i<people.length;i++) {
+  if (people[i].hasOwnProperty("yearOfDeath") === false) {
+    oldest = people[i].name;
+  } else if (people[i].hasOwnProperty("yearOfDeath") === true && (people[i].yearOfDeath)-(people[i].yearOfBirth) > oldestAge) {
+    oldestAge = (people[i].yearOfDeath-people[i].yearOfBirth)
+    oldest = people[i].name
+  }
+}
+
+
+console.log (oldestAge)
+console.log (oldest)
+
+}
+
+findTheOldest()
 
 // Do not edit below this line
 module.exports = findTheOldest;
